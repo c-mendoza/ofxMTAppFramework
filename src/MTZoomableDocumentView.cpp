2b731d5d7a9b936f9347cd3df5639eda9d091b91
c-mendoza <c@matadata.com>
2018-07-25 00:39:50 -0400

    MTZoomableDocumentView

---
+++ src/MTZoomableDocumentView.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Cristobal Mendoza on 7/24/18.
+//
+
+#include "MTZoomableDocumentView.hpp"
+
+MTZoomableDocumentView::MTZoomableDocumentView(const std::string& _name) : MTView(_name)
+{
+	setupDocumentRect(1920, 1080);
+}
+
+void MTZoomableDocumentView::draw()
+{
+	documentRectPath.draw();
+}
+
+
+void MTZoomableDocumentView::mouseDragged(int x, int y, int button)
+{
+	// TODO: Make view dragging code better
+	if (button == 2)
+	{
+//        ofLogVerbose("Dragging") << "contentMouse: " << x << " " << y;
+		auto dist = getScreenMouse() - getPrevScreenMouse();
+		setContentOrigin(getContentOrigin() + dist);
+//        shiftContentOrigin(glm::vec3(dist, 1));
+	}
+}
+
+void MTZoomableDocumentView::mouseScrolled(int x, int y, float scrollX, float scrollY)
+{
+	auto startPos = glm::vec2(getContentOrigin().x, getContentOrigin().y); // get a copy
+	auto startContentMouse = glm::clamp(getContentMouse(),
+										glm::vec2(0, 0),
+										glm::vec2(documentRect.getWidth(), documentRect.getHeight()));
+	float factor = (scrollY / 100.0f) + getContentScaleX();
+	factor = ofClamp(factor, 0.1, 10);
+//    ofLogVerbose("Scrolling") << "contentMouse: " << startContentMouse.x << " " << startContentMouse.y;
+	setContentScale(factor, factor);
+
+	// TODO: Improve the zoom, it is very finicky at the moment
+	auto newContentMouse = glm::vec2(getInvContentMatrix() * glm::vec4(getScreenMouse(), 1, 1));
+
+	newContentMouse = glm::clamp(newContentMouse,
+								 glm::vec2(0, 0),
+								 glm::vec2(documentRect.getWidth(), documentRect.getHeight()));
+//    setContentOrigin(getContentOrigin() + glm::vec3((newContentMouse.xy() - startContentMouse), 1));
+
+	auto diff = newContentMouse - startContentMouse;
+	shiftContentOrigin(diff);
+}
+
+void MTZoomableDocumentView::setupDocumentRect(float width,
+											   float height,
+											   const ofColor& backgroundColor,
+											   const ofColor& lineColor,
+											   float strokeWidth)
+{
+	documentRect = ofRectangle(0, 0, width, height);
+	documentRectPath.clear();
+	documentRectPath.setStrokeWidth(strokeWidth);
+	documentRectPath.setStrokeColor(lineColor);
+	documentRectPath.setColor(backgroundColor);
+	documentRectPath.setFilled(true);
+	documentRectPath.rectangle(documentRect);
+}
---
+++ src/MTZoomableDocumentView.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Cristobal Mendoza on 7/24/18.
+//
+
+#ifndef OFXMTAPPFRAMEWORK_MTZOOMABLEDOCUMENTVIEW_HPP
+#define OFXMTAPPFRAMEWORK_MTZOOMABLEDOCUMENTVIEW_HPP
+
+#include "MTView.hpp"
+
+/**
+ * @brief Adds zooming and panning behavior to a view using a mouse. Zooming is controlled by the scrollwheel,
+ * panning is done by dragging the mouse while pressing button 2 (which is typically the right mouse button).
+ *
+ * To use:
+ * 1. Make your view inherit from MTZoomableDocumentView.
+ * 2. Call setupDocumentRect as soon as you know what your document values should be.
+ * 3. If your view overrides mouseDragged or mouseScrolled, make sure to call this class's corresponding
+ * methods. For example:
+ *
+ * 		void yourViewClass::mouseDragged(int x, int y, int button) {
+ * 			MTZoomableDocumentView::mouseDragged(x, y, button);
+ * 			(your code here)
+ * 			...
+ * 		}
+ */
+class MTZoomableDocumentView : public MTView
+{
+public:
+	explicit MTZoomableDocumentView(const string& _name);
+	void draw() override;
+	void mouseDragged(int x, int y, int button) override;
+	void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
+	void setupDocumentRect(float width,
+						   float height,
+						   const ofColor& backgroundColor = ofColor::black,
+						   const ofColor& lineColor = ofColor::white,
+						   float strokeWidth = 1);
+private:
+	ofRectangle documentRect;
+	ofPath documentRectPath;
+};
+
+
+#endif //OFXMTAPPFRAMEWORK_MTZOOMABLEDOCUMENTVIEW_HPP
