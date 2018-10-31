c385b32dbc5c492e5dd9ef0591966aa02dab07b9
c-mendoza <c@matadata.com>
2018-10-30 22:33:00 -0400

    Adding version #defines for addon

--- src/ofxMTAppFramework.h
+++ src/ofxMTAppFramework.h
@@ -1,6 +1,5 @@
 //
 //  ofxMTAppFramework.h
-//  NervousStructureOF
 //
 //  Created by Cristobal Mendoza on 3/11/16.
 //
@@ -9,25 +8,16 @@
 #ifndef ofxMTAppFramework_h
 #define ofxMTAppFramework_h
 
-
-//This is sadly the only way that I can think of overriding "of..." globals.
-//This will cause ofGetMouseX and ofGetMouseY to return "local" view coordinates.
-//In addition to this define, you must enable Prefix headers and use the supplied .pch file.
-//#define OFXMTAPP_USE_LOCAL_MOUSE 1
+#define MTAF_VERSION_MAJOR 0
+#define MTAF_VERSION_MINOR 1
+#define MTAF_VERSION_PATCH 0
 
 
-//#ifdef OFXMTAPP_USE_LOCAL_MOUSE
-//	#define ofGetMouseX mtGetLocalMouseX
-//	#define ofGetMouseY mtGetLocalMouseY
-//#endif
-
-//#include "ofMain.h"
 #include "MTAppFrameworkUtils.hpp"
 #include "MTViewMode.hpp"
 #include "MTApp.hpp"
 #include "MTModel.hpp"
 #include "MTView.hpp"
 #include "MTWindow.hpp"
-
 
 #endif
